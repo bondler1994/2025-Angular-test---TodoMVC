4d62727e301be657c8342009f0f27ac8c6c7a3e8
WatsonLi <watsonli@t3ex-group.com>
2025-06-25 11:03:42 +0800

    add class and method, change component.ts todo to todoClass

--- 2025 Angular todoMVC/2_8範例檔/src/app/@module/todo-items.ts
+++ 2025 Angular todoMVC/2_8範例檔/src/app/@module/todo-items.ts
@@ -4,10 +4,15 @@ export interface Todo {
 }
 
 export class TodoClass {
-  Status: boolean;
-  Thing: string;
-  constructor(_thing: string, _status: boolean) {
-    this.Status = _status;
+  //class 需要初始直接赋值
+  //否则会报错：Property 'Status' has no initializer and is not definitely assigned
+  Status: boolean = false;
+  Thing: string = '';
+  constructor(_thing: string, _status: boolean = false) {
     this.Thing = _thing;
+    this.Status = _status;
+  }
+  toggle() {
+    this.Status = !this.Status;
   }
 }
--- 2025 Angular todoMVC/2_8範例檔/src/app/app.component.ts
+++ 2025 Angular todoMVC/2_8範例檔/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { Todo } from './app/@module/todo-items';
+// import { Todo } from './app/@module/todo-items';
+import { TodoClass } from './@module/todo-items';
 
 @Component({
   selector: 'app-root',
@@ -14,7 +15,7 @@ export class AppComponent {
   check1 = false;
   check2 = false;
 
-  todoDataList: Todo = [
+  todoDataList: TodoClass[] = [
     {
       Status: true,
       Thing: '第一件事情',
@@ -27,7 +28,7 @@ export class AppComponent {
       Status: false,
       Thing: '第三件事情',
     },
-  ];
+  ].map((data) => new TodoClass(data.Thing, data.Status));
 
   toggleAll() {
     this.toggleAllBtn = !this.toggleAllBtn;
@@ -36,18 +37,38 @@ export class AppComponent {
     });
   }
 
-  clickCheck(item: Todo) {
-    item.Status = !item.Status;
+  clickCheck(item: TodoClass) {
+    // 使用类的方法来切换状态
+    // 这样可以确保状态的切换逻辑在类内部处理
+    item.toggle();
   }
 
   delete(index: number) {
     this.todoDataList.splice(index, 1);
   }
 
+  // 直接使用字符串数组
+  // add(value: string) {
+  //   this.todoDataList.push({
+  //     Status: false,
+  //     Thing: value,
+  //   });
+  // }
+
+  // 使用 Todo 接口
+  // 这样可以确保添加的对象符合 Todo 接口的结构
+  // add(value: string) {
+  //   const todo: Todo = {
+  //     Status: false,
+  //     Thing: value,
+  //   };
+  //   this.todoDataList.push(todo);
+  // }
+
   add(value: string) {
-    this.todoDataList.push({
-      Status: false,
-      Thing: value,
-    });
+    // 使用 TodoClass 类来创建新的待办事项
+    // 这样可以确保添加的对象符合 TodoClass 的结构
+    // 并且可以使用类的方法和属性
+    this.todoDataList.push(new TodoClass(value));
   }
 }
